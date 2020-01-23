92adb3ae7c0d55d50c42632bd9eb8257af0edb06
Raphael Fleury <raphael.fleury@b2wdigital.com>
2020-01-23 17:21:18 -0300

    Adcionando números na lista

--- aula3/Miwok/app/src/main/java/com/example/miwok/NumbersActivity.java
+++ aula3/Miwok/app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -3,6 +3,11 @@ package com.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        int index = 0;
+
+        while (index < words.size()) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+
+            index++;
+        }
     }
 }
